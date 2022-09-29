e94ded72563a0c70bce05f4e6c0f2584dab9dcdf
Pavel <sebastian.pavel@capgemini.com>
2022-09-29 10:30:19 +0300

    updated api

--- pages/api/resources/index.js
+++ pages/api/resources/index.js
@@ -1,27 +1,33 @@
 import { cpu, drive, mem, os, netstat } from "node-os-utils";
+import * as si from "systeminformation";
 
 export default async function handler(req, res) {
-  res.status(200).json([
-    {
-      cpu: {
-        usage: await cpu.usage(1000),
-        load: cpu.loadavgTime(5).toFixed(2),
-        model: cpu.model(),
-        cpus: cpu.count(),
-      },
-    },
-    {
-      drive: await drive.info("/"),
-    },
-    {
-      memory: await mem.info(),
-    },
-    {
-      OS: {
-        type: await os.oos(),
-        ip: await os.ip(),
-        hostName: await os.hostname(),
-      },
-    },
-  ]);
+  si.cpuTemperature()
+    .then(async (data) => {
+      res.status(200).json([
+        {
+          cpu: {
+            usage: await cpu.usage(1000),
+            load: cpu.loadavgTime(5).toFixed(2),
+            model: cpu.model(),
+            cpus: cpu.count(),
+            temp: data,
+          },
+        },
+        // {
+        //   drive: await drive.info(),
+        // },
+        {
+          memory: await mem.info(),
+        },
+        {
+          OS: {
+            type: await os.oos(),
+            ip: await os.ip(),
+            hostName: await os.hostname(),
+          },
+        },
+      ]);
+    })
+    .catch((err) => res.send(err));
 }
