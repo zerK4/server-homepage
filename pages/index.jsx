9afba82aeee259b55177955d926866e8686274fa
Sebastian Pavel <zerka@Sebastians-MacBook-Pro.local>
2022-09-29 12:28:16 +0300

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,15 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get("/api/resources");
+      console.log(data);
+    };
+    getData();
+  }, []);
+
   return (
     <div className="underline">
       <h1 className="font-bold">Some text in here</h1>
